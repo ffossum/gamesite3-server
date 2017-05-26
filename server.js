bdd0599822ff75e20dfb764c74df4e51113a0e5c
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-05-26 16:50:03 +0200

    Add deepstream

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 /* @flow */
 const app = require('./src/app');
+const deepstream = require('deepstream.io-client-js');
 
-app.listen(3000);
+require('dotenv-safe').load();
+
+const deepstreamHost = process.env.DEEPSTREAM_HOST;
+const deepstreamUsername = process.env.DEEPSTREAM_USERNAME;
+const deepstreamPassword = process.env.DEEPSTREAM_PASSWORD;
+
+const port = process.env.SERVER_PORT;
+
+deepstream(deepstreamHost).login({
+  username: deepstreamUsername,
+  password: deepstreamPassword,
+});
+
+app.listen(port);
