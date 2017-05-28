06a7f1b10b57d411e5fecec0c29d956da9e810c8
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-05-28 12:59:06 +0200

    Redirect /registration if already logged in

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const jwt = require('koa-jwt');
 const apiRouter = require('./api/apiRouter');
+
+const jwtSecret = process.env.JWT_SECRET;
 
 const app = new Koa();
 const router = new Router();
 
 router.use('/api', apiRouter.routes(), apiRouter.allowedMethods());
+
+router.get(
+  '/registration',
+  jwt({ secret: jwtSecret, cookie: 'jwt', passthrough: true }),
+  async (ctx, next) => {
+    if (ctx.state.user) {
+      ctx.redirect('/');
+    }
+
+    await next();
+  }
+);
 
 router.get('*', ctx => {
   ctx.body = `<!doctype html>
