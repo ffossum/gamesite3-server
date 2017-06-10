8a05bdb5ec189a1a4d9a80f4bf885e2bb843a107
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-06-10 14:03:01 +0200

    Add createGame rpc provider

--- server.ts
+++ server.ts
@@ -1,8 +1,12 @@
 require("dotenv-safe").load();
 
 import * as deepstream from "deepstream.io-client-js";
+import { once } from "ramda";
 import * as winston from "winston";
 import app from "./src/app";
+import deepstreamApp from "./src/deepstreamApp";
+
+const initDeepstream = once(deepstreamApp);
 
 const deepstreamHost = process.env.DEEPSTREAM_HOST;
 const deepstreamUsername = process.env.DEEPSTREAM_USERNAME;
@@ -10,12 +14,20 @@ const deepstreamPassword = process.env.DEEPSTREAM_PASSWORD;
 
 const port = process.env.SERVER_PORT;
 
-const deepstreamClient = deepstream(deepstreamHost).login({
-  password: deepstreamPassword,
-  username: deepstreamUsername,
-});
-
+const deepstreamClient = deepstream(deepstreamHost);
 deepstreamClient.on("error", winston.error);
+
+deepstreamClient.login(
+  {
+    password: deepstreamPassword,
+    username: deepstreamUsername,
+  },
+  (success, data) => {
+    if (success) {
+      initDeepstream(deepstreamClient);
+    }
+  },
+);
 
 app.listen(port);
 winston.info(`Server listening on port ${port}`);
