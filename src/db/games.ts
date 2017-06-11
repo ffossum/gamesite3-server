f186c16764cff4be7b5c3bd48ba02c06c9595b60
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-06-11 19:11:08 +0200

    Add creation timestamp to games

--- src/db/games.ts
+++ src/db/games.ts
@@ -4,6 +4,7 @@ import { UserId } from "./users";
 type GameId = string;
 
 export interface IGame {
+  createdTime: string;
   host: UserId;
   id: GameId;
   players: UserId[];
@@ -13,8 +14,9 @@ const gamesById = new Map<GameId, IGame>();
 
 export async function createGame(host: UserId): Promise<IGame> {
   const gameId = shortid.generate();
-
+  const createdTime = new Date().toISOString();
   return {
+    createdTime,
     host,
     id: gameId,
     players: [],
